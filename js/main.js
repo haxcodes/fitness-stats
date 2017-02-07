7f0bb374f9df76ca38b1307ad0388205bc2a69a4
Aonghas Anderson <aonghas@gmail.com>
2017-02-07 00:00:26 +0000

    basic steps this month

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
+var date = new Date();
+var monthString = date.getFullYear() + ('0' + (date.getMonth() + 1)).slice(-2)
+
 $.ajax({
-  url: "https://hax.codes/fit/api/getCombined"
+  url: "https://hax.codes/fit/api/steps/daily/" + monthString
 }).done(function(data) {
-  $('#data').html(data);
+  setTimeout(function(){
+    $('.total-steps').html(totalSteps(data));
+  }, 0);
 });
+
+var totalSteps = function(data) {
+  return _.reduce(_.filter(data, function(day) { 
+            return _.where(day.summary, {activity: 'walking' }).length > 0 }), function(memo, day) {
+              return _.findWhere(day.summary, {activity: 'walking'}).steps + memo;
+            }, 0);
+};
+
+window.odometerOptions = {
+  auto: false, 
+  selector: '.animate-numbers', 
+  format: '(,ddd).dd', 
+  duration: 3000, 
+  theme: 'default' 
+};
