f06de8318ad830fc19e8a6ac8761bf27274437be
Aonghas Anderson <aonghas@gmail.com>
2017-02-08 22:58:21 +0000

    added moving footpath

--- js/main.js
+++ js/main.js
@@ -1,34 +1,47 @@
-var date = new Date();
-var monthString = date.getFullYear() + '-' + (date.getMonth() + 1)
-var dateString = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + (date.getDate());
-
-$(document).ready(function(){
-    $("#watch-container").addClass('enlarge');
-});
-
-
-$.ajax({
-  url: "https://hax.codes/fit/api/steps/daily/" + monthString
-}).done(function(data) {
-  setTimeout(function(){
-    $('.month-steps').html(totalSteps(data));
-  }, 2000);
-});
-
-$.ajax({
-  url: "https://hax.codes/fit/api/steps/daily/" + dateString
-}).done(function(data) {
-  setTimeout(function(){
-    $('.day-steps').html(totalSteps(data));
-  }, 2000);
-});
-
+// Main functions for fitness stats
 var totalSteps = function(data) {
   return _.reduce(_.filter(data, function(day) { 
             return _.where(day.summary, {activity: 'walking' }).length > 0 }), function(memo, day) {
               return _.findWhere(day.summary, {activity: 'walking'}).steps + memo;
             }, 0);
 };
+
+var init = function() {
+  var date = new Date();
+  var monthString = date.getFullYear() + '-' + (date.getMonth() + 1)
+  var dateString = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + (date.getDate());
+
+  
+  $(".footpath").addClass('moved');
+
+  setTimeout(function() {
+    $("#watch-container").addClass('enlarge');
+
+    $.ajax({
+      url: "https://hax.codes/fit/api/steps/daily/" + monthString
+    }).done(function(data) {
+      setTimeout(function(){
+        $('.month-steps').html(totalSteps(data));
+      }, 2000);
+    });
+
+    $.ajax({
+      url: "https://hax.codes/fit/api/steps/daily/" + dateString
+    }).done(function(data) {
+      setTimeout(function(){
+        $('.day-steps').html(totalSteps(data));
+      }, 2000);
+    });
+  }, 1000);
+
+
+
+}
+
+$(document).ready(function(){
+  document.body.style.background = '#99' + _.times(2, function(n) { return _.random(160,255).toString(16) }).join('');
+  init();
+});
 
 window.odometerOptions = {
   auto: false, 
@@ -37,5 +50,3 @@ window.odometerOptions = {
   duration: 3000, 
   theme: 'default' 
 };
-
-document.body.style.background = '#99' + _.times(2, function(n) { return _.random(160,255).toString(16) }).join('');
